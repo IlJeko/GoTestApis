40ee0cb06f34ee56404b1cefccfbbc3f6e3c8420
IlJeko <60620724+IlJeko@users.noreply.github.com>
2025-03-31 01:28:13 +0200

    Added register, update and delete user and get one product enpoints

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "******"
+	password = "Eros2724@1"
 )
 
 var db *sql.DB
@@ -52,7 +53,11 @@ func main() {
 	// Setup routes
 	router := gin.Default()
 	router.GET("/products", getProducts)
+	router.GET("/products/:id", getProductByID)
 	router.POST("/login", LoginHandler)
+	router.POST("/register", Register)
+	router.PUT("/users/:id", UpdateUser) //used PUT instead of PATCH to pass the entire entity and simplify the code and not to use an ORM
+	router.DELETE("/users/:id", DeleteUser)
 
 	router.Run("localhost:8080")
 }
@@ -95,6 +100,58 @@ func getProducts(c *gin.Context) {
 	c.JSON(http.StatusOK, products)
 }
 
+func getProductByID(c *gin.Context) {
+	id := c.Param("id")
+
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing authorization header"})
+		return
+	}
+
+	tokenString := authHeader[len("Bearer "):]
+
+	if err := VerifyToken(tokenString); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	var product Product
+	err := db.QueryRow("SELECT * FROM products WHERE id = $1", id).Scan(&product.ID, &product.Name, &product.Stock, &product.Price)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		return
+	}
+	c.JSON(http.StatusOK, product)
+}
+
+func Register(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	validate := validator.New()
+
+	err := validate.Struct(u)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Validation error: check inserted data"})
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", u.Username, u.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+}
+
 func LoginHandler(c *gin.Context) {
 	var u User
 	if err := c.ShouldBindJSON(&u); err != nil {
@@ -102,18 +159,14 @@ func LoginHandler(c *gin.Context) {
 		return
 	}
 
-	// Create a new validator instance
 	validate := validator.New()
 
-	// Validate the User struct
 	err := validate.Struct(u)
 	if err != nil {
-		// Validation failed, handle the error
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Validation error: check inserted data"})
 		return
 	}
 
-	// Query the user by username
 	var dbPassword string
 	err = db.QueryRow("SELECT password FROM users WHERE username = $1", u.Username).Scan(&dbPassword)
 	if err != nil {
@@ -125,19 +178,52 @@ func LoginHandler(c *gin.Context) {
 		return
 	}
 
-	// Check if password matches
 	if u.Password != dbPassword {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
 		return
 	}
 
-	// Create JWT token
 	tokenString, err := CreateToken(u.Username)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Token generation failed"})
 		return
 	}
 
-	// Success
 	c.String(http.StatusOK, tokenString)
+}
+
+func UpdateUser(c *gin.Context) {
+	userID := c.Param("id")
+
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	_, err := db.Exec("UPDATE users SET username = $1, password = $2 WHERE id = $3", u.Username, u.Password, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User updated successfully"})
+}
+
+func DeleteUser(c *gin.Context) {
+	userID := c.Param("id")
+
+	result, err := db.Exec("DELETE FROM users WHERE id = $1", userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete user"})
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
 }
